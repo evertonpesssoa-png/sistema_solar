9d58f318835e6bd54e595498c1211102c67ef8b9
Everton Pessoa <evertonpesssoa@gmail.com>
2026-04-26 20:28:52 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ function createStars() {
     star.className = "star";
 
     const size = Math.random() * 2;
+    const duration = Math.random() * 5 + 2; // 2s a 7s
+    const delay = Math.random() * 5;
 
     star.style.width = size + "px";
     star.style.height = size + "px";
     star.style.top = Math.random() * 100 + "%";
     star.style.left = Math.random() * 100 + "%";
-    star.style.opacity = Math.random();
+
+    star.style.animationDuration = duration + "s";
+    star.style.animationDelay = delay + "s";
 
     container.appendChild(star);
   }
